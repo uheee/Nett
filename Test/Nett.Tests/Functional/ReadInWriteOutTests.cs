8b2dd6b80ff77d3649ac8e4bb5ce407eca7f312d
Philipp Haiden <phaiden@ranorex.com>
2019-11-21 16:12:45 +0100

    <WIP>

--- Test/Nett.Tests/Functional/ReadInWriteOutTests.cs
+++ Test/Nett.Tests/Functional/ReadInWriteOutTests.cs
@@ -17,5 +17,26 @@ namespace Nett.Tests.Functional
 
             written.TrimEnd().Should().Be(input);
         }
+
+        [Fact]
+        public void GivenMutlipleDottedKeys_WritesTheTomlCorrectly_SoThatItCanBeReadCorrectlyAfterwards()
+        {
+            // Arrange
+            const string input = @"
+[app]
+value1 = ""string""
+obj.a = 1
+obj.b = 2
+";
+
+            var tml = Toml.ReadString(input);
+
+            // Act
+            var written = Toml.WriteString(tml);
+
+            // Assert
+            var readBack = Toml.ReadString(written);
+            readBack.Get<TomlTable>("obj").Get<int>("b").Should().Be(2);
+        }
     }
 }
