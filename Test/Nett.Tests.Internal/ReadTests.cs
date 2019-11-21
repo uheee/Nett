9c70789eb21613a97941d3203e8a050eee3759ef
Philipp Haiden <phaiden@ranorex.com>
2019-11-21 16:38:14 +0100

    <WIP>: hmmmm

---
+++ Test/Nett.Tests.Internal/ReadTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Nett.Tests.Internal
+{
+    public class ReadTests
+    {
+        [Fact]
+        public void Fooxxxxyy()
+        {
+            // Arrange
+            const string tml = @"
+x = 2
+a.a = 3
+a.b = 4";
+
+            // Act
+            var read = Toml.ReadString(tml);
+
+            // Assert
+            read.Get<TomlTable>("a").TableType.Should().Be(TomlTable.TableTypes.Dotted);
+        }
+    }
+}
